c930cfeedeeb2c064e2118c1445de7f347ade2ed
s184805 <s184805@DESKTOP-7B3BTE4.home>
2019-04-27 12:37:33 +0200

    Start på repo

---
+++ JavaApplication22/src/Ludo_funktion/Felt.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ludo_funktion;
+
+/**
+ *
+ * @author s184805
+ */
+public class Felt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
