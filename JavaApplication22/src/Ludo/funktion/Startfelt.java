956acf354cbf2587e933695329a23d36b738cf60
Valdemar Landberg <Valdemar Landberg@LAPTOP-PLFGULU9>
2019-04-29 13:05:27 +0200

    Startfelt landet opdatering

--- JavaApplication22/src/Ludo/funktion/Startfelt.java
+++ JavaApplication22/src/Ludo/funktion/Startfelt.java
@@ -4,49 +4,61 @@
  * and open the template in the editor.
  */
 package Ludo.funktion;
+
 import Ludo.enheder.*;
 import java.util.ArrayList;
+
 /**
  *
  * @author s184805
  */
 public class Startfelt implements Felt {
+
     final String farve;
     final int feltnr;
     int antalBrikker;
     ArrayList<Brik> brikker;
-    
-    public Startfelt (String spFarve, int spFeltnr){
+    Regler regler;
+
+    public Startfelt(String spFarve, int spFeltnr) {
         this.farve = spFarve;
         this.feltnr = spFeltnr;
-        this.brikker = new ArrayList<Brik> ();
-        for(int i = 0; i < 4; i++){
-            this.brikker.add(null);
-        }
+        this.brikker = new ArrayList<Brik>();
+        Regler regler = new Regler();
+
     }
-    
-    
+
     @Override
-    public void landet(Brik brikInd){
-        if(this.farve.equals(brikInd.getFarve())){
-            
-            
-            if (brikker.isEmpty() || )
-            brikker.add(brikInd.getID(), brikInd);
-        }
-        else{
-            if(brikker.isEmpty()){
-                brikker.add(brikInd.getID(), brikInd);
+    public void landet(Brik brikInd) {
+        if (brikker.isEmpty()) {
+            brikker.add(brikInd);
+        } else {
+            Brik tmp = brikker.get(0);
+            if (tmp.getFarve().equals(this.farve)) {
+                if (brikInd.getFarve().equals(tmp.getFarve())) {
+                    brikker.add(brikInd);
+                } else {
+                    regler.kamp(true, brikker.size(), tmp, brikInd);
+                }
+
+            } else {
+                if (brikInd.getFarve().equals(tmp.getFarve())) {
+                    brikker.add(brikInd);
+                } else {
+                    if (brikInd.getFarve().equals(this.farve)) {
+                        
+                    } else {
+                        regler.kamp(false, brikker.size(), tmp, brikInd);
+                    }
+                }
             }
-            else{
-                brikker
-            }
+
         }
     }
-    
+
     @Override
-    public void forlader(Brik brikUd){
-        
+    public void forlader(Brik brikUd) {
+
     }
-    
+
 }
