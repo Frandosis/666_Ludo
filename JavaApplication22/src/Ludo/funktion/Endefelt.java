5d64b2e558699a521d8b6650004f28439971eede
jcall <jcall@LAPTOP-H27PU74N.clients.wireless.dtu.dk>
2019-04-29 11:52:52 +0200

    ny class buffer + Endefelt aendring

--- JavaApplication22/src/Ludo/funktion/Endefelt.java
+++ JavaApplication22/src/Ludo/funktion/Endefelt.java
@@ -5,10 +5,35 @@
  */
 package Ludo.funktion;
 
+import Ludo.enheder.*;
 /**
  *
  * @author s184805
  */
 public class Endefelt implements Felt {
+        String farve;
+        int feltnr;
+        int antalBrikker;
+        int tmp;
+        String out;
+ 
+    public void moveToBuffer(){
     
+        
+        
+    }
+        
+        
+    public String checkWin() {
+        tmp = Buffer.getAntalBuffer;
+        
+        if(tmp== 4){
+            out = ("win");
+           return out;
+        }
+        else{
+        out = ("continue");
+        }
+        return out;
+    }
 }
