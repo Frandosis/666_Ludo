d043557fdebc2cb413fe78dcd2bf3be2f13d62e7
Valdemar Landberg <Valdemar Landberg@LAPTOP-PLFGULU9>
2019-05-02 11:40:16 +0200

    Opdatering

--- JavaApplication22/src/Ludo/funktion/Banefelt.java
+++ JavaApplication22/src/Ludo/funktion/Banefelt.java
@@ -4,16 +4,37 @@
  * and open the template in the editor.
  */
 package Ludo.funktion;
+import Ludo.enheder.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  *
  * @author s184805
  */
 public class Banefelt implements Felt {
-
-    /**
-     * @param args the command line arguments
-     */
+  
+    final int feltnr;
+    Queue<Brik> brikker;
+    Regler regler;
+    boolean angrebVandt;
+    
+    public Banefelt(int spFeltnr){
+        this.feltnr = spFeltnr;
+        this.brikker = new LinkedList<>();
+        angrebVandt = false;
+        
+    }
+    
+   @Override
+   public void landet(Brik brikInd){
+       
+   }
+   
+   @Override
+   public void forlader(Brik brikUd){
+       
+   }
 
     
 }
